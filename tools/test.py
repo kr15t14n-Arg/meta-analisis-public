1678c156146d683a3d9ab9553623ce159e2e8263
Cristian Gabriel Ríos <tu-correo@ejemplo.com>
2025-11-11 21:58:06 -0300

    Version 0.5 - 11/11/25 Mejoras en la UI Mejoras en la presentacion de resultados Mejoras en el informe de salida Limpieza de codigo en general

--- tools/test.py
+++
@@ -1,6 +0,0 @@
-from tools.metadata_utils import parse_decimal_from_exif_gps
-
-lat = parse_decimal_from_exif_gps("32 deg 54' 10.48\" S")
-lon = parse_decimal_from_exif_gps("60 deg 44' 4.91\" W")
-
-print(lat, lon)
